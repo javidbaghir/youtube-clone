d02be782202c24fab3f2beafc82b974d58fae68c
javidbaghir <c.bagirov01@gmail.com>
2025-05-07 20:24:27 +0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import Video from "./pages/video/Video";
-import Sidebar from "./components/sidebar/Sidebar"; // əlavə et
-import { useState } from "react";
+import Sidebar from "./components/sidebar/Sidebar";
+import { useEffect, useState } from "react";
 
 function App() {
   const [menu, setMenu] = useState(true);
+  const location = useLocation();
 
   const toggleSidebar = () => {
     setMenu(!menu);
   };
+
+  useEffect(() => {
+    if (location.pathname.includes("/video")) {
+      setMenu(true); 
+    } else {
+      setMenu(true); 
+    }
+  }, [location]);
 
   return (
     <>
